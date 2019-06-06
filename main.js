a45f947d8d321e72464900ff4f2a5bc51d37e4ed
a7madd94 <44666121+a7madd94@users.noreply.github.com>
2019-06-06 08:21:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function goToLink(){
-  window.location.href = 'https://t.hrtyi.com/s21zdvwwg0?url_id=0&aff_id=34028&offer_id=3785&source=Instagram&nopop=1';
+  window.location.href = 'https://t.hrtyi.com/qy3wck8lj4?url_id=0&aff_id=34028&offer_id=3664&bo=2779,2778,2777,2776,2775';
 }
