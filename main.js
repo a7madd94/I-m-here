fa16b04a40caf4396c59d405958186d38a5ed4f1
a7madd94 <44666121+a7madd94@users.noreply.github.com>
2019-06-07 04:24:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function goToLink(){
-  window.location.href = 'https://t.hrtyi.com/qy3wck8lj4?url_id=0&aff_id=34028&offer_id=3664&bo=2779,2778,2777,2776,2775';
+  window.location.href = 'https://t.frtyi.com/fbuql7aetc?url_id=15593&aff_id=34028&offer_id=2994&bo=2779,2778,2777,2776,2775';
 }
