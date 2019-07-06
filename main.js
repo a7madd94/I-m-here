d0324159cbcff571e3ab3fe799331bf04eb9af62
a7madd94 <44666121+a7madd94@users.noreply.github.com>
2019-07-06 03:08:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function goToLink(){
-  window.location.href = 'https://t.frtyi.com/9x4fb5vvwg?url_id=12015&aff_id=34028&offer_id=5125&bo=2779,2778,2777,2776,2775';
+  window.location.href = 'http://track.cpacashoffers.com/click?pid=536&offer_id=27&sub1=myprivatestuff';
 }
